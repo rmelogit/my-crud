d0d1612cde438ccf7c4636ccf0fb652495c9883f
rmelogit <raquelmelo07@hotmail.com>
2019-05-28 00:37:43 -0300

    modal quebrou a tela

--- src/components/tasks/list/List.js
+++ src/components/tasks/list/List.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
 import Table from 'react-bootstrap/Table';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 class List extends Component {
 
-    async editaCliente(cliente) {
+    close(){
+        this.setState({ showModal: false });
+    }
 
-        await fetch(`http://localhost:3000/alterar/${cliente.id}`,
+    open(cliente){
+        this.setState({ showModal: true, id: cliente.id, nome: cliente.nome, sobrenome: cliente.sobrenome, data: cliente.data, sexo: cliente.sexo });
+    }
+
+    async editaCliente(id,nome,sobrenome,data,sexo) {
+        await fetch(`http://localhost:3000/alterar/${id}`,
             {
                 method: 'PUT',
                 headers: {
@@ -15,11 +26,10 @@ class List extends Component {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(
-                    {nome: cliente.nome, sobrenome: cliente.sobrenome, data: cliente.data, sexo: cliente.sexo}
+                    { nome: nome, sobrenome: sobrenome, data: data, sexo: sexo}
                 )
             }
         );
-
         this.props.listaClientes();
     }
 
@@ -28,6 +38,23 @@ class List extends Component {
             await fetch(`http://localhost:3000/deletar/${cliente.id}`, {method: 'DELETE'});
             this.props.listaClientes();
         }
+    }
+
+    mudaNome(e) {
+        var value = e.target.value;
+        this.setState({nome: value});
+    }
+    mudaSobrenome(e) {
+        var value = e.target.value;
+        this.setState({sobrenome: value});
+    }
+    mudaData(e) {
+        var value = e.target.value;
+        this.setState({data: value});
+    }
+    mudaSexo(e) {
+        var value = e.target.value;
+        this.setState({sexo: value});
     }
 
     render() {
@@ -44,7 +71,7 @@ class List extends Component {
                                     <td className="col-md-10">{cliente.data}</td>
                                     <td className="col-md-10">{cliente.sexo}</td>
                                     <td>
-                                        <a className="edit" href="" onClick={() => this.editaCliente(cliente)} size="lg">
+                                        <a className="edit" href="" onClick={this.open.bind(this,cliente)} size="lg">
                                             <FontAwesomeIcon icon="pen-alt"/>
                                         </a>
                                     </td>
@@ -60,6 +87,29 @@ class List extends Component {
                     </Card.Body>
                 </Card>
 
+        <Modal show={this.state.showModal}
+               onHide={this.close}
+               keyboard={true}>
+            <Modal.Header closeButton>
+                <Modal.Title>Editar Cliente</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form.Control type="text" placeholder="Nome" value={this.state.nome || ''} onChange={e => this.mudaNome(e)} />
+                <Form.Control type="text" placeholder="Sobrenome" value={this.state.sobrenome || ''} onChange={e => this.mudaSobrenome(e)} />
+                <Form.Control type="date" placeholder="Data de Nascimento" value={this.state.data || ''} onChange={e => this.mudaData(e)} />
+                <Form.Control type="text" placeholder="Sexo" value={this.state.sexo || ''} onChange={e => this.mudaSexo(e)} />
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={this.close}>
+                    Close
+                </Button>
+                <form onSubmit={this.editaCliente(this.state.id,this.state.nome,this.state.sobrenome,this.state.data,this.state.sexo)}>
+                    <Button variant="dark" type="submit">
+                        Editar
+                    </Button>
+                </form>
+            </Modal.Footer>
+        </Modal>
             </div>
         );
     }
